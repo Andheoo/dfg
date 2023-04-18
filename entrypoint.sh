fe097d79f65c4cb165bb26d9da454b0214b96ebe
Andheoo <118645459+Andheoo@users.noreply.github.com>
2023-04-18 04:37:13 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ DIR_CONFIG="/etc/v2ray"
 DIR_RUNTIME="/usr/bin"
 DIR_TMP="$(mktemp -d)"
 
-uuid=ad806487-2d26-4636-98b6-ab85cc8521f7
-mypath=/mylove-fsd79sa
+uuid=58f8f341-99d5-4949-ba14-92c6e7f3c7b3
+mypath=/mylove-fsd123sa
 myport=8080
 
 
